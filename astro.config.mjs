7ca9e5f6d53837301adabb0ac9c458b05a9a2a83
Joel Robert Justiawan <JOELwindows7@users.noreply.github.com>
2022-10-21 21:58:31 +0700

    before we install community plugins though
    let's push 1st after all these plugins within Astro official documentation.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+import image from '@astrojs/image';
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  site: 'https://perkedel.netlify.app',
+  integrations: [mdx(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), sitemap(), prefetch(), partytown()]
 });
