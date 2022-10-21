9e8054dad938f63c7d0bce6c9c2ae7b26b1e8362
Joel Robert Justiawan <JOELwindows7@users.noreply.github.com>
2022-10-22 02:05:29 +0700

    okay hold
    try compile first.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 import { defineConfig } from 'astro/config';
+
 import mdx from "@astrojs/mdx";
 import image from '@astrojs/image';
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
-
 import partytown from "@astrojs/partytown";
+import robotsTxt from 'astro-robots-txt';
+import embeds from 'astro-embed/integration';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://perkedel.netlify.app',
-  integrations: [mdx(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap(), prefetch(), partytown()]
+    site: 'https://perkedel.netlify.app',
+    integrations: [mdx(), image({
+        serviceEntryPoint: '@astrojs/image/sharp'
+    }), sitemap(), prefetch(), partytown(), embeds(), robotsTxt()],
+    legacy: {
+        astroFlavoredMarkdown: false,
+    },
 });
