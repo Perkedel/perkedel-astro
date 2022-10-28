2fbcb55a23ed501b7778c75e517dac196e8efeca
Joel Robert Justiawan <JOELwindows7@users.noreply.github.com>
2022-10-28 17:28:46 +0700

    weteston

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000/",
+    // baseUrl: "http://localhost:3000/",
+    baseUrl: "https://perkedel.netlify.app",
     supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
