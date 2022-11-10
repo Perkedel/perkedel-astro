2d1cff789341b8068e86722bf9bc81139c5977b9
Joel Robert Justiawan <JOELwindows7@users.noreply.github.com>
2022-11-10 16:03:20 +0700

    screw this! use regular JSON instead
    this is not easily working. I NEED HELP!!!

--- public/main.js
+++ public/main.js
@@ -314,18 +314,23 @@ let daWord = [
 //     daWord = data.daWord;
 //     console.log(daWord);
 // });
+getJSONP('/generateWord.json', function(data) {
+
+    daWord = data.daWord;
+    console.log(daWord);
+});
 
 
 function regenerateWord() {
     document.getElementById('generateWord').innerHTML = daWord[Math.floor(Math.random() * daWord.length)];
 }
 
-$(function() {
-    $.getJSON('/generateWord.jsonc', function(data) {
-        daWord = data.daWord;
-        console.log(daWord);
-    });
-});
+// $(function() {
+//     $.getJSON('/generateWord.jsonc', function(data) {
+//         daWord = data.daWord;
+//         console.log(daWord);
+//     });
+// });
 regenerateWord();
 
 /*
