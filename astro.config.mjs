b0f6ab7c35781714f9e14821d485b5405ed53c17
Joel Robert Justiawan <JOELwindows7@users.noreply.github.com>
2023-02-05 21:41:28 +0700

    sogmit v2
    astro now is v2 yeyeye

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,7 +16,14 @@ export default defineConfig({
         // image({
         //     serviceEntryPoint: '@astrojs/image/sharp',
         // }),
-        sitemap(), prefetch(), partytown(), embeds(), robotsTxt()
+        sitemap(), prefetch(), partytown(), embeds(), robotsTxt(
+            {
+                sitemap: 'https://perkedel.netlify.app/sitemap-0.xml'
+                transform(content) {
+                    return `# Robots.txt for Perkedel ASTR.\n# use https://github.com/alextim/astro-lib/tree/main/packages/astro-robots-txt#readme to generate this.\n#by JOELwindows7\n#PerkedelTechnologies\n#GNU GPL v3\n\n${content}`;
+                },
+            }
+        ),
     ],
     markdown: {
         remarkPlugins: [
