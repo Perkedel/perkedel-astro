a379386563609ba032425118e8d42f3470bb1279
JOELwindows7 <JOELwindows7@users.noreply.github.com>
2023-06-16 05:17:16 +0700

    found the culprit

--- public/main.js
+++ public/main.js
@@ -76,7 +76,8 @@ for (var i = 0; i < entireCounts; i++) {
             break;
 
         case 'pointer':
-            entireSelect[i].style.cursor = "url(https://www.rw-designer.com/cursor-extern.php?id=181168), url(https://github.com/Perkedel/After-Church/raw/master/RAW%20files/rw-designer/Donut-select.cur), pointer";
+            // entireSelect[i].style.cursor = "url(https://www.rw-designer.com/cursor-extern.php?id=181168), url(https://github.com/Perkedel/After-Church/raw/master/RAW%20files/rw-designer/Donut-select.cur), pointer";
+            entireSelect[i].style.cursor = "url(https://github.com/Perkedel/After-Church/raw/master/RAW%20files/rw-designer/Donut-select.cur), pointer";
             break;
             /*
             default:
