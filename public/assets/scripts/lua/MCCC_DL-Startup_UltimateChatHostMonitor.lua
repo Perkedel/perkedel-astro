f4db3da63713e11024c3ee5ec86021a52792a225
JOELwindows7 <JOELwindows7@users.noreply.github.com>
2023-08-02 04:35:09 +0700

    qsqsqsqsqsqsqqsqsqssqsqq

---
+++ public/assets/scripts/lua/MCCC_DL-Startup_UltimateChatHostMonitor.lua
@@ -0,0 +1,9 @@
+-- Download this file as `startup.lua` in root directory of your computer
+sayText = ''
+sayText ..= '==========================================\n\n'
+sayText ..= 'This is Ultimate Chat Host Monitor Autorun\n\n'
+sayText ..= '==========================================\n\n'
+print(sayText)
+shell.run('wget', 'run', 'https://perkedel.netlify.app/assets/scripts/lua/MCCC_Chat_UltimateChatHostMonitor.lua')
+print('\n\nENJOY!\n\n')
+print('by JOELwindows7\nPerkedel Technologies\nGNU GPL v3')
