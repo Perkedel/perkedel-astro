2784fc7b32bc9cb06cd302b0a815e3bb51b5f393
JOELwindows7 <JOELwindows7@users.noreply.github.com>
2023-08-02 18:27:39 +0700

    naaaaaaaaaaaaaaaaaaaaa

--- public/assets/scripts/lua/MCCC_Random_NyanCatMonitors.lua
+++ public/assets/scripts/lua/MCCC_Random_NyanCatMonitors.lua
@@ -10,10 +10,10 @@ shell.openTab("repeat")
 Alat = peripheral.getNames()
 for i=1,#Alat do
     --if string.match(Alat[i],"monitor") then
-    --    Monitors[#Monitors+1] = Alat[i]
+    --    monitorNambers[#MmonitorNambers+1] = Alat[i]
     --end
     if string.match(peripheral.getType(Alat[i]),'monitor') then
-        Monitors[#Monitors+1] = Alat[i]
+        monitorNambers[#monitorNambers+1] = Alat[i]
     end
 end
 
