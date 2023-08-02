006e181da350d60693ebede1129f91900d1619bb
JOELwindows7 <JOELwindows7@users.noreply.github.com>
2023-08-02 20:51:28 +0700

    aqhahahahahahahah

--- public/assets/scripts/lua/MCCC_Random_NyanCatMonitors.lua
+++ public/assets/scripts/lua/MCCC_Random_NyanCatMonitors.lua
@@ -22,7 +22,14 @@ print("Connects through modem! Not sides")
 print("youtu.be/QdgZZwM0Beg\n")
 print("pastebin.com/"..pastebinID.."\n")
 print("Downloading Nyan Cat...")
-shell.run("pastebin","get "..pastebinID.." "..fileName)
+if not fs.exist(filename) then
+    shell.run("pastebin","get "..pastebinID.." "..fileName)
+else
+    print('Nyan Cat is already downloaded')
+end
+print("\n\nRun on self new tab then monitors")
+shell.openTab(filename)
+print("now get this to other monitors")
 for i=1,#monitorNambers do
     --shell.openTab("monitor", monitorNambers[i].." pastebin run "..pastebinID.."")
 	shell.openTab("monitor", monitorNambers[i].." "..fileName)
