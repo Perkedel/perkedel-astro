0fc8a8adabb79726be99382404ae9d1092527e85
JOELwindows7 <JOELwindows7@users.noreply.github.com>
2025-09-30 15:39:53 +0700

    aq

---
+++ src_curl/index.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+# what what what
+#
+# https://stackoverflow.com/a/5947802
+# https://style.ysap.sh/
+# https://en.wikipedia.org/wiki/ANSI_escape_code
+# https://github.com/bahamas10/ysap/blob/main/website/theme
+#
+# run your page, then pipe the out to respective file!
+# 
+# by JOELwindows7
+# Perkedel Technologies
+# GNU AGPL v3
+
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo "Hello World"
+echo "${YELLOW}Perkedel NTLF+ANSI"
+echo "${GREEN}https://perkedel.netlify.app"
+
+cowsay "idk how to curl lol"
